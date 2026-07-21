8ef73bda854e00f8f92beee272bdfc908d2f4d76
Liviu Ionescu <ilg@livius.net>
2026-07-21 08:28:02 +0300

    move to semihosting.cpp

---
+++ src/semihosting.cpp
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * Copyright (c) 2023-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can be
+ * obtained from https://opensource.org/licenses/mit.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include <micro-os-plus/semihosting.h>
+
+// ----------------------------------------------------------------------------
+
+// SWI numbers and reason codes for RDI (Angel) monitors.
+#define AngelSWIInsn "swi"
+// The order is important, since Cortex-M defines both.
+#if defined(__thumb__)
+#define AngelSWI 0xAB
+#elif defined(__arm__)
+#define AngelSWI 0x123456
+#else
+#error "Unsupported architecture."
+#endif
+
+  micro_os_plus_semihosting_response_t
+  micro_os_plus_semihosting_call_host (
+      int reason, micro_os_plus_semihosting_param_block_t* arg)
+  {
+    micro_os_plus_semihosting_response_t value;
+    __asm__ volatile(
+
+        " mov r0, %[rsn] \n"
+        " mov r1, %[arg] \n"
+        " " AngelSWIInsn " %[swi] \n"
+        " mov %[val], r0"
+
+        : [val] "=r"(value) /* Outputs */
+        : [rsn] "r"(reason), [arg] "r"(arg), [swi] "i"(AngelSWI) /* Inputs */
+        : "r0", "r1", "r2", "r3", "ip", "lr", "memory", "cc"
+        // Clobbers r0 and r1, and lr if in supervisor mode
+    );
+
+    // Accordingly to page 13-77 of ARM DUI 0040D other registers
+    // can also be clobbered. Some memory positions may also be
+    // changed by a system call, so they should not be kept in
+    // registers. Note: we are assuming the manual is right and
+    // Angel is respecting the APCS.
+    return value;
+  }
+
+// ----------------------------------------------------------------------------
